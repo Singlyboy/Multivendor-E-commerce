57dae152df225e1b11a7f139ffed0eced11f419f
Sarol-Hossain <sarolsagar@gmail.com>
2024-10-06 13:50:27 +0600

    fromtend home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeControler;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/',[HomeController::class,'home'])->name('dashboard');
+Route::get('/',[HomeControler::class,'home'])->name('home');
